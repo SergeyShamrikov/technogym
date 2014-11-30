736059b05f52994d697cd230b9e94581f0b573c8
Сергей <sergey.shamrikov@gmail.com>
2014-11-30 23:36:01 +0200

    правки и дополнения по странице about.html

--- js/script.js
+++ js/script.js
@@ -11,47 +11,15 @@
     wow.init();
 
 
-    $(window).on('scroll',function(){
-      var headerBottomOffset = $('.header_bottom').offset().top,
-          sidebarOffset = $('.main_content_left').offset().top,
-          rightBottomOffset = $('#main_content_right_bottom').offset().top,
-          sidebarHeight = $('#sidebar').height(),
-          windowScrollTop = $(window).scrollTop(),
-          sidebarStatic = rightBottomOffset-sidebarHeight-60,
-          sidebarPadding = rightBottomOffset-sidebarOffset-sidebarHeight,
-          sidebarFixed = windowScrollTop + 60;
-      if(headerBottomOffset<=windowScrollTop){
-        $('#nav').addClass('active');
-        $('#sidebar').css({
-          'padding-top': 0
-        });
-      }
-      else{
-        $('#nav').removeClass('active');
-      }
-      if(sidebarOffset<=sidebarFixed && windowScrollTop<=sidebarStatic){
-        $('#sidebar').addClass('active');
-      }
-      else if(sidebarStatic<=windowScrollTop){
-        $('#sidebar').removeClass('active');
-        $('#sidebar').css({
-          'padding-top': sidebarPadding
-        });
-      }
-      else{
-        $('#sidebar').removeClass('active');
-        $('#sidebar').css({
-          'padding-top': 0
-        })
-      }
-    });
+   
 
     // flaxslider
-    $('.flexslider').flexslider({
-      animation: "slide"
-    });
-
-
+    if ($('.flexslider').length){
+      $('.flexslider').flexslider({
+        animation: "slide"
+      });
+    }
+      
     // anhor
     $('.anhor').on('click',function () {
       var elementClick = $(this).attr("href"),
@@ -65,26 +33,6 @@
       return false;
     });
 
-    // current class scroll 
-    $(window).scroll(function(){
-      $(".nav_content").each(function () {
-        var _this = $(this);
-        var window_top = $(window).scrollTop();
-        var div_ = $(this).attr('id');
-        var div_top = $(this).offset().top;
-        var div_1 = "#"+div_;
-          if (window_top > div_top -100){
-              $('.nav_content').removeClass('active');
-              $(_this).addClass('active');
-              $('.sidebar_list_link').parent().removeClass('current');
-              $('.sidebar_list_link[href="'+div_1+'"]').parent().addClass('current');
-          }
-          else{
-              $('.sidebar_list_link[href="'+div_1+'"]').removeClass('current');
-              };
-      });
-    });
-
     // modal
     $('.popup_open').on('click',function(){
       var modal = $(this).data("modal");
@@ -95,4 +43,5 @@
     $("a[rel=gallery_group]").fancybox({
       'padding' :40
     });
+
   });
